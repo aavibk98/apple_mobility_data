7af1d891331b22721bf012e50f09ab3f67a82fa8
Aavikumar Bhakta <aavibk@Aavikumars-MacBook-Pro.local>
2021-09-21 11:50:11 -0700

    Fix variable name based on lintr recommendation

--- code/02_count_up_counties_cities.R
+++ code/02_count_up_counties_cities.R
@@ -1,4 +1,4 @@
-# the goal of this script is to use the dplyr package to count up the 
+# the goal of this script is to use the dplyr package to count up the
 # number of cities and counties in a given state that have detailed
 # Apple mobility data available and then eventually to save it to a csv
 
@@ -6,10 +6,10 @@
 # September 21, 2021
 # abhakta2@dons.usfca.edu
 
-# load the package "dplyr" 
+# load the package "dplyr"
 library("dplyr")
 
-# we would like to get a count of the number of cities and the 
+# we would like to get a count of the number of cities and the
 # number of counties in a given state that have driving mobility data
 # The input to this will be the output of the previous script
 # namely the state csv files that were already subsetted
@@ -19,10 +19,10 @@ input_file_name <- "output/applemobilitytrends-2021-09-17_Michigan.csv"
 state_data <- read.csv(input_file_name)
 
 # starting off with dplyr chains
-count_of_cities_counties_by_transportation_type <- state_data %>% 
+count_cities_counties_by_type <- state_data %>%
   select(geo_type, region, transportation_type) %>%
   group_by(geo_type, transportation_type) %>%
   tally()
 
-write.csv(count_of_cities_counties_by_transportation_type,
+write.csv(count_cities_counties_by_type,
           "output/michigan_citiese_counties_counts.csv")
